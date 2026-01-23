5f730b9694b126060885f4c1bc33e8d0d3669fa0
杨春辉 <13749847+yangchunhui123@user.noreply.gitee.com>
2026-01-23 18:23:15 +0800

    2026/1/23

--- agent/workflow/node/node_config/LLMConfig.py
+++ agent/workflow/node/node_config/LLMConfig.py
@@ -10,6 +10,7 @@ class LLMConfig:
     system_prompt: str
     model: str = field(default="gpt-4.1")
     temperature: float = field(default=0.0)
+    mcp_list: list = field(default_factory=list)
     openai_api_key: str = field(default=os.getenv("OPENAI_API_KEY"))
     openai_api_base: str = field(default=os.getenv("OPENAI_API_BASE"))
     need_structure_output: bool = False  # 是否需要结构化输出
